882668102f3b1f1f987a91b12e381e67c8a8da0a
xjs1919 <605162215@qq.com>
2016-09-30 10:33:32 +0800

    删除

--- src/main/java/com/github/xjs/util/CollectionUtil.java
+++ src/main/java/com/github/xjs/util/CollectionUtil.java
@@ -38,43 +38,6 @@ public class CollectionUtil {
         return false;
     }
 	
-	public interface Has<T, F>{
-        public boolean has(T bean, F fieldValue);
-    }
-
-    public static <T, F> T find(List<T> collection, F fieldValue, Has<T, F> has){
-        for(T elem : collection){
-            if(has.has(elem, fieldValue)){
-                return elem;
-            }
-        }
-        return null;
-    }
-
-    public interface Eq<T>{
-        public boolean eq(T t1, T t2);
-    }
-
-    public static <T> boolean contains(List<T> collection,T elem){
-        if(elem == null){
-            return false;
-        }
-        Eq<T> eq = (t1, t2)->t1.equals(t2);
-        return contains(collection, elem, eq);
-    }
-
-    public static <T> boolean contains(List<T> collection,T elem, Eq<T> eq){
-        if(elem == null){
-            return false;
-        }
-        for(T t : collection){
-            if(eq.eq(elem, t)){
-                return true;
-            }
-        }
-        return false;
-    }
-    
     public static boolean isEmpty(Collection<?> collection) {
 		return (collection == null) || collection.isEmpty();
 	}
