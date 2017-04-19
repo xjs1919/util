6bc2056def5808266b8696b430d73cb659f6e784
xjs1919 <xjs@163.com>
2017-04-19 18:39:54 +0800

    IDUtil

--- src/main/java/com/github/xjs/util/IDUtil.java
+++ src/main/java/com/github/xjs/util/IDUtil.java
@@ -3,139 +3,128 @@
  */
 package com.github.xjs.util;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author 605162215@qq.com
  *
- * 2016年8月19日 下午2:39:56
- * 参考：http://chuansong.me/n/2459549
+ * 2016年8月19日 下午2:39:56 
+ * 参考：
+ * http://www.cnblogs.com/ppli/p/6542915.html
+ * http://chuansong.me/n/2459549
+ * 1位最高位
+ * 8位机器号，256个节点
+ * 14位序列号，毫秒内并发16383
+ * 41位时间戳，可用69年
  */
 public class IDUtil {
-private IDUtil(){}
-	
+
+	/** 开始时间截 ，birthday of Cathy */
+	private final long start = 1441313880000L;
+
+	/** 生成序列的掩码 */
+	private final long sequenceMax = (1 << 14) - 1;
+
+	/** 毫秒内序列(0~sequenceMax) */
+	private long sequence = 0L;
+
+	/** 上次生成ID的时间截 */
+	private long lastTimestamp = -1L;
+
+	private static IDUtil instance = new IDUtil();
+
+	private IDUtil(){
+		
+	}
+
+	public static long getId(int serverId) {// 6位，64个机器
+		return instance.nextId(serverId);
+	}
+
 	/**
+	 * 获得下一个ID (该方法是线程安全的)
 	 * 
-	 * birthday of Cathy
+	 * @return SnowflakeId
 	 */
-	private static final long BIRTHDAY_OF_CATHY = 1441313880000L;
-	/**
-	 * 序列号
-	 */
-	private static final AtomicInteger UNIQUE_ID = new AtomicInteger(0);
-	/**
-	 * 1毫秒以内最大的并发数 = 序列号的最大值
-	 */
-	private static final int MAX_ID = 2 << 7 - 1;
-	/**
-	 * 随机
-	 */
-
-	/**
-	 * 表名
-	 * */
-	public static enum Table{
-		User(1),
-		Role(2),
-		Menu(3),
-		;
-		private int id;
-		private Table(int id){
-			this.id = id;
+	public synchronized long nextId(int serverId) {
+		long timestamp = timeGen();
+		// 如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
+		if (timestamp < lastTimestamp) {
+			throw new RuntimeException(String.format(
+					"Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
 		}
-		public int getId(){
-			return this.id;
-		}
-	}
-	
-	/**
-	 * 通过这个方法向外暴漏
-	 * */
-	public static long getId(Table table, int server) {
-		return getId(table.getId(), server);
-	}
-	
-	private static long getId(int table, int server) {
-		int sn = getSN();
-		long id = System.currentTimeMillis() - BIRTHDAY_OF_CATHY;// 毫秒数,30年
-		id = id << 2;               // 预留
-		id = id << 7 | server;      // 机器号               127
-		id = id << 7 | table;       // 业务线              127
-		id = id << 7 | sn;          // 毫秒内的并发   127
-		//40 + 2 + 7 + 7 + 7 = 63，     最高位留出来，否则就是负数了
-		return id;
-	}
-
-	private static int getSN() {
-		int num = UNIQUE_ID.incrementAndGet();
-		if (num >= MAX_ID) {//单线程一个毫秒可能会生成超过127个，时间部分增加下，保证严格递增
-			if(UNIQUE_ID.compareAndSet(MAX_ID, 1)){
-				try{
-					Thread.sleep(1);
-				}catch(Exception e){
-					e.printStackTrace();
-				}
-				return num;
-			}else{
-				UNIQUE_ID.set(1);
-				try{
-					Thread.sleep(1);
-				}catch(Exception e){
-					e.printStackTrace();
-				}
-				return getSN();
+		// 如果是同一时间生成的，则进行毫秒内序列
+		if (lastTimestamp == timestamp) {
+			sequence = (sequence + 1) & sequenceMax;
+			// 毫秒内序列溢出
+			if (sequence == 0) {
+				// 阻塞到下一个毫秒,获得新的时间戳
+				timestamp = tilNextMillis(lastTimestamp);
 			}
+		} else {// 时间戳改变，毫秒内序列重置
+			sequence = 0L;
 		}
-		return num;
+		// 上次生成ID的时间截
+		lastTimestamp = timestamp;
+		// 移位并通过或运算拼到一起组成64位的ID
+		return ((timestamp - start) << 22) | (serverId << 14) | sequence;
 	}
 
+	/**
+	 * 阻塞到下一个毫秒，直到获得新的时间戳
+	 * 
+	 * @param lastTimestamp
+	 *            上次生成ID的时间截
+	 * @return 当前时间戳
+	 */
+	protected long tilNextMillis(long lastTimestamp) {
+		long timestamp = timeGen();
+		while (timestamp <= lastTimestamp) {
+			timestamp = timeGen();
+		}
+		return timestamp;
+	}
+
+	/**
+	 * 返回以毫秒为单位的当前时间
+	 * 
+	 * @return 当前时间(毫秒)
+	 */
+	protected long timeGen() {
+		return System.currentTimeMillis();
+	}
+	
 	public static void main(String[] args) throws Exception {
-		//1.多线程测试
-//		long start = System.currentTimeMillis();
-//		final ConcurrentHashMap<String, String> map = new ConcurrentHashMap<String, String>();
-//		int threadCount = 10000;//单机1万并发没有问题
-//		final CountDownLatch endLatch = new CountDownLatch(threadCount);
-//		final CountDownLatch startLatch = new CountDownLatch(1);
-//		Thread[] arr = new Thread[threadCount];
-//		for (int i = 0; i < threadCount; i++) {
-//			arr[i] = new Thread(new Runnable() {
-//				public void run() {
-//					try {
-//						startLatch.await();
-//					} catch (Exception e) {
-//						e.printStackTrace();
-//					}
-//					String num = "" + getId(1, 1);
-//					// System.out.println(Thread.currentThread().getName()+":"+num);
-//					map.put(num, num);
-//					endLatch.countDown();
-//				}
-//			});
-//		}
-//		for (int i = 0; i < threadCount; i++) {
-//			arr[i].start();
-//		}
-//		startLatch.countDown();
-//		endLatch.await();
-//		System.out.println(map.size());
-//		long end = System.currentTimeMillis();
-//		System.out.println("use:" + (end - start));
-		//2.单线程测试
-		List<Long> list = new ArrayList<Long>();
-		for(int i=0;i<5000;i++){
-			long id = getId(1, 1);
-			list.add(id);
+		long start = System.currentTimeMillis();
+		final ConcurrentHashMap<String, String> map = new ConcurrentHashMap<String, String>();
+		int threadCount = 1000;//单机1万并发没有问题
+		final CountDownLatch endLatch = new CountDownLatch(threadCount);
+		final CountDownLatch startLatch = new CountDownLatch(1);
+		Thread[] arr = new Thread[threadCount];
+		for (int i = 0; i < threadCount; i++) {
+			arr[i] = new Thread(new Runnable() {
+				public void run() {
+					try {
+						startLatch.await();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					for(int i=0;i<200;i++){
+						String num = "" + getId(1);
+						map.put(num, num);
+					}
+					endLatch.countDown();
+				}
+			});
 		}
-		for(int i=0;i<list.size()-1;i++){
-			long front = list.get(i);
-			long back = list.get(i+1);
-			if(front > back){
-				System.out.println(front+","+back);
-			}
+		for (int i = 0; i < threadCount; i++) {
+			arr[i].start();
 		}
-		System.out.println("over");
+		startLatch.countDown();
+		endLatch.await();
+		System.out.println(map.size());
+		long end = System.currentTimeMillis();
+		System.out.println("use:" + (end - start));
 	}
-
 }
