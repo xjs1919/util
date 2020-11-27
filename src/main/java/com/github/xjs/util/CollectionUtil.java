21b433ffa8b4c383481b4d5bd5134cba33f617ee
xujiashuai <xujiashuai@keruyun.com>
2020-11-27 11:16:38 +0800

    split batchExecute

--- src/main/java/com/github/xjs/util/CollectionUtil.java
+++ src/main/java/com/github/xjs/util/CollectionUtil.java
@@ -3,11 +3,13 @@
  */
 package com.github.xjs.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -18,6 +20,41 @@ import java.util.stream.Collectors;
  * 2016年8月12日 下午5:27:50
  */
 public class CollectionUtil {
+
+
+	/**
+	 * 按照batchSize批量执行，比如: 数据库批量更新的场景，防止一次太多，可以拆分多次执行
+	 * */
+	public static <T> void batchExecute(List<T> list, int batchSize, Consumer<? super List<T>> consumer){
+		List<List<T>> listList = split(list, batchSize);
+		if(listList == null || listList.size() <= 0){
+			return;
+		}
+		listList.stream().forEach(consumer);
+	}
+
+	/**
+	 * list按照batchSize拆分
+	 * */
+	public static <T> List<List<T>> split(List<T> list, int batchSize){
+		if(batchSize <= 0){
+			throw new RuntimeException("batch size 必须大于0");
+		}
+		if(list == null || list.size() <= 0){
+			return new ArrayList<>(0);
+		}
+		int totalSize = list.size();
+		int resultSize = (totalSize + batchSize - 1)/batchSize;
+		List<List<T>> ret = new ArrayList<>(resultSize);
+		for(int i=0; i<resultSize; i++){
+			List<T> subList = new ArrayList<>(batchSize);
+			for(int j=i*batchSize; j<list.size() && j<(i+1)*batchSize; j++){
+				subList.add(list.get(j));
+			}
+			ret.add(subList);
+		}
+		return ret;
+	}
 	
 	public static <T> T find(List<T> collection, Predicate<T> predicate){
 		Optional<T> optional = collection.stream().filter(predicate).findFirst();
